12b2797cc34f95bac3080b7da8c5614b240c6ced
Hamish Barnes <hamishbarnes@hamishs-MacBook-Pro.local>
2020-05-14 20:39:05 +0100

    init

---
+++ src/database.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "1": {
+    "name": "little gorilla",
+    "birthday": "08-17"
+  },
+  "2": {
+    "name": "Derp Flerp",
+    "birthday": "02-09"
+  },
+  "3": {
+    "name": "Boottle McFoodle",
+    "birthday": "10-23"
+  },
+  "4": {
+    "name": "Tengo Crepes",
+    "birthday": "07-06"
+  },
+  "5": {
+    "name": "Terry the Dolphin",
+    "birthday": "04-10"
+  }
+}
